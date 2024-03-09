33e6ec6f3321c562faa49a7376cea9f4686e2fed
Shevlulek <shevlulek@gmail.com>
2024-03-09 09:40:10 +0200

    pyton01.py

---
+++ pyton01.py
@@ -0,0 +1,14 @@
+number = 10 # int
+name = "andrey" # str
+surname = "shevchenko"
+b = True # bool
+f = 54.34 # float
+import random
+l = [12, True, "test", 54.543, name]
+print(l)
+l.append(random.randit(10,57))
+print(l)
+l.pop(2)
+print(l)
+
+print(name + surname)
